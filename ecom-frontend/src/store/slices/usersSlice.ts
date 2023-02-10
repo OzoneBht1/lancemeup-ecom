5af92d332f2fe73062847969272e3efffd51ba2b
OzoneBht1 <np03cs4s210036@heraldcollege.edu.np>
2023-02-10 17:49:22 +0545

    Configure redux store and add user slice

---
+++ ecom-frontend/src/store/slices/usersSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "..";
+import { IUser } from "../../types/types";
+
+interface UserState {
+  user: IUser | null;
+}
+
+const initialState: UserState = {
+  user: null,
+};
+
+export const usersSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<IUser>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logout } = usersSlice.actions;
+
+export default usersSlice.reducer;
